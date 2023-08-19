d9887425bbc07cbf98b7301eecf3c3d628ad33c5
Renato Albuquerque <renatomalb83@gmail.com>
2023-08-19 13:34:59 -0300

    added url in js file

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function share(){
 		navigator.share({
 			title: 'Júlio Paulino Imóveis - Cartão de Visita Digital',
 			text: 'Aluguel, Compra, Venda e Atividades Afins.',
-			url: ''
+			url: 'https://cartaodevisita-juliopaulinoimoveis.vercel.app/'
 		})
 		.then(() => console.log('Successful share'))
 		.catch((error) => console.log('Error sharing', error));
